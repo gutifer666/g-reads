a2fc0e430b45fc9b140a088f1d9c1c9c5c720552
Javier Gutiérrez <gutifer666@gmail.com>
2020-09-10 09:56:01 +0200

    Connect with API and show list

--- README.md
+++ README.md
@@ -22,3 +22,8 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+<!-- 1:11 -->
+
+AIzaSyCcFIyzEY0clAz89EnesNIKGI7ItRTxqzw
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="section">
+      <h1 class="title">Google Books API</h1>
+      <div class="container card-grid">
+        <div class="card" v-for="book in books" :key="book.industryIdentifiers[0].identifier">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-128x128">
+                  <!-- <img v-if="!imageLinks.thumbnail" src="https://books.google.com/books/content?id=OpnZBAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" alt="Image Fake">
+                  <img v-else :src="book.imageLinks.thumbnail" alt="Real Image"> -->
+                  <img src="https://books.google.com/books/content?id=OpnZBAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api" alt="Real Image">
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-4">{{ book.title }}</p>
+                <p class="subtitle is-6">{{ book.authors[0] }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+  data() {
+    return {
+      books: [],
+    };
+  },
+
+  methods: {
+    load() {
+      fetch(
+        "https://www.googleapis.com/books/v1/volumes?q=inauthor:escohotado&key=AIzaSyCcFIyzEY0clAz89EnesNIKGI7ItRTxqzw"
+      )
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          this.books = this.books.concat(data.items.map(item => item.volumeInfo));
+          console.log(this.books);
+        });
+    },
+  },
+
+  created() {
+    this.load();
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.card-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+  gap: 1em;
 }
 </style>
+
+
